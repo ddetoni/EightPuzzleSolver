fe537913be458ee4d2fc825ba9416c12c38e5d4b
douglas <douglas_detoni@hotmail.com>
2014-04-25 16:49:49 -0300

    Method get_next_state() created.

--- src/solver.py
+++ src/solver.py
@@ -1,19 +1,22 @@
 class EightPuzzleSolver:
     b_state_list = None
-    
+
     def __init__(self, initial_state):
         self.b_state_list = [BoardState(initial_state, 0)]
-    
+
     def start(self):
         print self.b_state_list[0].state
-        
+
 class BoardState:
     state = None
     index_father = None
-    
+
     def __init__(self, state, index):
         self.state = state
-        self.index_father = index    
+        self.index_father = index
+
+    def get_next_states():
+    	pass
 
 if __name__ == '__main__':
     puzzle = EightPuzzleSolver([1,0,8,2,4,7,5,3,6])
